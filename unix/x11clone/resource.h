8da70e33167fc52fc855a92a91e43714c25047dc
Peter Åstrand (astrand) <astrand@cendio.se>
2017-10-25 14:46:03 +0200

    Removed resource.h; only for Windows

--- unix/x11clone/resource.h
+++
@@ -1,7 +0,0 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by x11clone.rc
-//
-#define IDR_MANIFEST                    1
-#define IDI_ICON                        101
-
